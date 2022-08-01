63cbcec34ebf60b99f08f6de98e6c3a8a954cfd8
Emmanuel Sylvester <emmanuelsylvester@macbook-pro.myfiosgateway.com>
2022-07-31 20:05:30 -0400

    Intial idea

---
+++ mockdata/user.js
@@ -0,0 +1,58 @@
+export const user = {
+    "username": "emansyl",
+    "firstName": "Emmanuel",
+    "lastName": "Sylvester" ,
+    "email" : "emmanuel.sylvester22@gmail.com",
+    "songs": [
+        {
+            "title" : "Crocodile Teeth",
+            "artist" : "Skilibeng"
+        },
+        {
+            "title" : "My Type",
+            "artist" : "Popcaan"
+        },
+        {
+            "title" : "Forever",
+            "artist" : "Vybz Kartel"
+        },
+        {
+            "title" : "Hot Topic",
+            "artist" : "Govana"
+        },
+        {
+            "title" : "London",
+            "artist" : "Skeng"
+        },
+        {
+            "title" : "Crocodile Teeth",
+            "artist" : "Skilibeng"
+        },
+        {
+            "title" : "Crocodile Teeth",
+            "artist" : "Skilibeng"
+        },
+        {
+            "title" : "Crocodile Teeth",
+            "artist" : "Skilibeng"
+        },
+        {
+            "title" : "Crocodile Teeth",
+            "artist" : "Skilibeng"
+        }
+    ],
+    "playlists" : [
+        {
+            "name" : "DanceHall Hits",
+            "createdBy" : "emansyl"
+        },
+        {
+            "name" : "Summer Vibes",
+            "createdBy" : "emansyl"
+        },
+        {
+            "name" : "Soca Party",
+            "createdBy" : "emansyl"
+        }
+    ]
+}
--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,57 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
-import { getSortedPostsData } from '../lib/posts'
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
+import Date from "../components/date";
+import Song from "../components/song";
+import { getUserData } from "../lib/getUser";
 
-export default function Home({ allPostsData }) {
+import { getSortedPostsData } from "../lib/posts";
+
+export async function getStaticProps() {
+  const allUserData= getUserData()
+  console.log(allUserData)
+
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+      allUserData
+    },
+  };
+}
+
+export default function Home({ allPostsData, allUserData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>"Hi, I'm Emmanuel. Checkout my music taste</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              {title}
-              <br />
-              {id}
-              <br />
-              {date}
+        <h2 className={utilStyles.headingLg}>Top Songs</h2>
+        <ol className={utilStyles.list}>
+          {allUserData.songs.slice(0,5).map(({title,artist }) => (
+            <li className={utilStyles.listItem} key={title}>
+              <Song title = {title} artist = {artist} />
+
             </li>
           ))}
-        </ul>
+        </ol>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Favourite Playlists</h2>
+        <ol className={utilStyles.list}>
+          {allUserData.playlists.slice(0,5).map(({name,createdBy }) => (
+            <li className={utilStyles.listItem} key={name}>
+              <Song title = {name} artist = {createdBy} />
+
+            </li>
+          ))}
+        </ol>
       </section>
     </Layout>
-  )
-}
-
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
+  );
 }
